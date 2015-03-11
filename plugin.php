4d671efe2f171d9071e835af4f8eef6d30a08c15
James Golovich <james@gnuinter.net>
2015-03-11 01:39:24 -0700

    Move require up to make lib available earlier

--- plugin.php
+++ plugin.php
@@ -6,11 +6,15 @@ Author Name: James Golovich
 License: GPL2
 */
 
-add_action( 'init', 'pritect_exampleloader_with_submodule' );
+add_action( 'plugins_loaded', 'pritect_exampleloader_with_submodule_startup' );
+add_action( 'init', 'pritect_exampleloader_with_submodule_go' );
 
-function pritect_exampleloader_with_submodule() {
+function pritect_exampleloader_with_submodule_startup() {
 
 	require_once 'includes/Pritect-ExampleLoader/example-loader.php';
+}
+
+function pritect_exampleloader_with_submodule_go() {
 	$PAK = pritect_example_loader( array(), '1.0.0' );
 	$PAKnewest = pritect_example_loader();
 	$a = 1;
